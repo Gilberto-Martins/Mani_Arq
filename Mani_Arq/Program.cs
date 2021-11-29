5a1054b64586154ec09a9d57501b6438a29d32c5
Gilberto Martins <gilbertomartins643@gmail.com>
2021-11-29 18:02:11 -0300

    Projeto iniciado

---
+++ Mani_Arq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mani_Arq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
